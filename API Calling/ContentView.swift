d312224b981eb7385953ba365e6308015ce68468
Nerto Kageh <kagehnerto@yahoo.com>
2022-02-16 10:35:29 -0600

    Prototype. This code has errors.

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var IMDB = [IMDB]()
+    @State private var showingAlert = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+            NavigationView
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
